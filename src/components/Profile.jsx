677e203a384a30cc31d84f5eb729f9bbe8d5f615
lawyerh <lawyerh_01@yahoo.com>
2018-05-25 06:55:58 -0700

    changing workstations

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import NavBar from './home/nav_bar.jsx';
 import '../styles/profile.css';
 
 class Profile extends Component {
     render(){
         return(
             <div>
+                <NavBar />
                 Profile Section
             </div>
         )
